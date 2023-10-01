1894987466bc872f636211f7c97177a048c8b19b
Dcoding15 <debrishibiswas2003@gmail.com>
2023-10-01 14:38:38 +0530

    updated

--- Infinity hotel/inf_hotel/infinite_hotel/urls.py
+++ Infinity hotel/inf_hotel/infinite_hotel/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('registration/', views.page1, name='registration'),
+    path('', views.page1, name='registration'),
     path('login/',views.page2, name='login')
 ]
